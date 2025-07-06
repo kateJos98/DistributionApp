44e606bbe2851c11980cb7b7603824a9e5a90981
kateJos98 <katyjoseth@gmail.com>
2025-07-06 11:44:50 -0500

    probar Terraform correctamente

--- infra/envs/QA/main.tf
+++ infra/envs/QA/main.tf
@@ -26,7 +26,7 @@ module "qa_auth_login" {
   subnet_id         = var.subnet_id_1
   security_group_id = module.qa_sg_nginx.id
   name              = "qa-auth-login"
-  private_key_path    = var.private_key_path
+  private_key_path  = var.private_key_path
   user_data         = file("../../scripts/userdata-auth_login.sh")
 }
 
@@ -49,14 +49,14 @@ module "qa_authorization_login" {
 # NGINX Gateway
 # ============================
 module "qa_nginx_gateway" {
-  source                = "../../modules/nginx_gateway"
-  ami                   = var.ami
-  instance_type         = var.instance_type
-  key_name              = var.key_name
-  subnet_id             = var.subnet_id_1
-  security_group_id     = module.qa_sg_nginx.id
-  name                  = "qa-nginx-gateway"
-  auth_login_ip         = module.qa_auth_login.public_ip
+  source                 = "../../modules/nginx_gateway"
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id_1
+  security_group_id      = module.qa_sg_nginx.id
+  name                   = "qa-nginx-gateway"
+  auth_login_ip          = module.qa_auth_login.public_ip
   authorization_login_ip = module.qa_authorization_login.public_ip
 }
 

648f9eb03aefe58d39807c4edbbedb1e2b0af0fe
kateJos98 <katyjoseth@gmail.com>
2025-07-06 12:18:41 -0500

    probar Terraform

--- infra/envs/QA/main.tf
+++ infra/envs/QA/main.tf
@@ -49,13 +49,13 @@ module "qa_authorization_login" {
 # NGINX Gateway
 # ============================
 module "qa_nginx_gateway" {
-  source                 = "../../modules/nginx_gateway"
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = var.key_name
-  subnet_id              = var.subnet_id_1
-  security_group_id      = module.qa_sg_nginx.id
-  name                   = "qa-nginx-gateway"
+  source            = "../../modules/nginx_gateway"
+  ami               = var.ami
+  instance_type     = var.instance_type
+  key_name          = var.key_name
+  subnet_id         = var.subnet_id_1
+  security_group_id = module.qa_sg_nginx.id
+  name              = "qa-nginx-gateway"
   microservices = {
     "auth-login"          = "${module.qa_auth_login.public_ip}:8001"
     "authorization-login" = "${module.qa_authorization_login.public_ip}:8002"

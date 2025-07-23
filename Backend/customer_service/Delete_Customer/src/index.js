1ff7516ec962f7790aacc5c5b0d84240fe46c3ed
kateJos98 <katyjoseth@gmail.com>
2025-07-22 22:33:48 -0500

    index.js  correction

--- Backend/customer_service/Delete_Customer/src/index.js
+++ Backend/customer_service/Delete_Customer/src/index.js
@@ -1,9 +1,6 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import { CustomerController } from './Controllers/Customer.controller.js';
-
-dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());

bca234e68e38286649af908287cb4276e437c559
kateJos98 <katyjoseth@gmail.com>
2025-07-28 17:33:25 -0500

    Refactor:test update-customer #14

--- Backend/customer_service/Update_Customer/src/Config/Database.php
+++ Backend/customer_service/Update_Customer/src/Config/Database.php
@@ -6,7 +6,7 @@ use PDOException;
 
 class Database {
     public static function connect(): PDO {
-        
+        error_log("Conectando a MySQL.....);
         $host = getenv('DB_HOST');
         $port = getenv('DB_PORT');
         $db   = getenv('DB_NAME');

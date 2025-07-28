3b277c1a013a5f2dbefe6f2f73d47b2db26b9586
kateJos98 <katyjoseth@gmail.com>
2025-07-28 14:07:32 -0500

    Refactor:test update-customer #8

--- Backend/customer_service/Update_Customer/src/Config/Database.php
+++ Backend/customer_service/Update_Customer/src/Config/Database.php
@@ -5,10 +5,10 @@ use PDO;
 
 class Database {
     public static function connect(): PDO {
-        $host = $_ENV['DB_HOST'] ?? ;
-        $db = $_ENV['DB_NAME'] ?? ;
-        $user = $_ENV['DB_USER'] ?? 'root';
-        $pass = $_ENV['DB_PASS'] ?? '';
+        $host = $_ENV['DB_HOST'];
+        $db = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
+        $pass = $_ENV['DB_PASS'];
 
         return new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
